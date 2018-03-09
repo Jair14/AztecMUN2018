0648e45af9ce7638ba805a149dfe12c661afbe23
Jair14 <hernandez_yo14@outlook.es>
2018-03-09 17:05:56 -0600

    Validaciones añadidas

--- js/ajax.js
+++ js/ajax.js
@@ -17,14 +17,64 @@ function insertar(){
     validacion = false;
     respuesta += "Ingrese su matricula<br>";
   }
+  if (grupo == "" || grupo == null || grupo == " ") {
+    validacion = false;
+    respuesta += "Ingrese su grupo<br>";
+  }
+  if (comite == "" || comite == null || comite == " ") {
+    validacion = false;
+    respuesta += "Ingrese un comité<br>";
+  }
+  if (pais == "" || pais == null || pais == " ") {
+    validacion = false;
+    respuesta += "Seleccione un país<br>";
+  }
+  if (mail == "" || mail == null || mail == " ") {
+    validacion = false;
+    respuesta += "Ingrese su correo electrónico<br>";
+  }
+  if (numero == "" || numero == null || numero == " ") {
+    validacion = false;
+    respuesta += "Ingrese su número telefónico<br>";
+  }
+  if(nombre.length < 10){
+    validacion = false;
+    respuesta += "Su nombre debe de contener por lo menos 10 caracteres<br>";
+  }
+  if (matricula.length > 8 || matricula.length < 8) {
+    validacion = false;
+    respuesta += "La matricula debe contener solo 8 caracteres numericos<br>";
+  }
+  var expr = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  if (!expr.test(mail)){
+      validacion = false;
+      respuesta += "Ingrese un correo con el formato válido<br>";
+  }
+  if(numero < 5500000000){
+    validacion = false;
+    respuesta += "El número debe comenzar por 55<br>";
+  }
+  if (numero.length < 10 || numero.length > 10) {
+    validacion = false;
+    respuesta += "El número telefónico debe tener 8 digitos más la clave<br>";
+  }
+
+
   if (validacion) {
     $.ajax({
     type: 'post',
     url: 'data.php',
     data: dataen,
+    beforeSend: function() {
+        $("#formulario").slideUp();
+        $("#respuesta").html("<div class='progress'><div class = 'indeterminate'></div></div><br>Espere un momento mientras sus datos son enviados...");
+    },
     success: function(respond) {
       $("#formulario").slideUp();
       $("#respuesta").html(respond);
+    },
+    error : function(xhr, status) {
+        $("#respuesta").html("Lo sentimos, hemos tenido un problema): <br> Intentelo más tarde");
     }
   });
   }else{
